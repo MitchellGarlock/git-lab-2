1586de6b28013bcf1fca97a274d31f8eeef3ce77
MitchellGarlock <mg351615@ohio.edu>
2019-09-04 17:01:32 -0400

    Lab Program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Mitchell Garlock
+ *        Date: 9/4/2019
+ * Description: Test code for GitHub
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout <<"Hello Git!" << endl;
+    return 0;
+}// main
